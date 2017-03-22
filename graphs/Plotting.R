ea89f3dc5fa9948ff32cf9a4743c3d706e8bee2f
DarcyShu <darcyshu@gmail.com>
2017-03-22 16:07:56 -0400

    Delete Unnecessary Codes and change the codes into a function code.

--- graphs/Plotting.R
+++ graphs/Plotting.R
@@ -1,21 +1,13 @@
 #HW9 PlotByTime<-function(time, count)
 
-install.packages("ggplot2") #plot
-library(ggplot2)
-FinalDate<-AggregateByCase(dat$ARRESTTIME,dat$COUNCIL_DISTRICT)
-Time<-gsub("-","",FinalDate$time)
-plot(factor(Time), FinalDate$DistrCount, 
-     col="blue", 
-     lwd=3, 
-     xlabel="Arrstdate", 
-     ylabel="DistrCount", 
-     main="Count_Crime", 
-     type = "l")
-
 install.packages("ggplot2") #ggplot
 library(ggplot2)
-qplot(Time,DistrCount,data=FinalDate,main="Count_Crime")
-ggplot(FinalDate,aes(x=Time,y=FinalDate$DistrCount))+
-  geom_point()+
-  theme(axis.text.x = element_text(size=3,angle=90))
+PlotbyTime<-function(time,count){
+  dat<-data.frame(time,count)
+  ggplot(dat, aes(x=time, y=count))+
+    geom_point()+
+    theme(axis.text.x = element_text(size=6, angle=90))+
+    ggtitle("Number of Council Districts Involved Across Time")+
+    labs(x="Year",y="Number of Council Districts")
+}
 
